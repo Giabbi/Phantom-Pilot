dbd8d2f97ab55771b161b621c4547c52e11e5f7c
Giancarlo Umberto Ambrosino <129298577+Giabbi@users.noreply.github.com>
2024-01-20 18:28:49 -0800

    Comments Starter.py

--- Phantom Pilot Starter.py
+++ Phantom Pilot Starter.py
@@ -11,9 +11,9 @@ screen.tracer(0)
 # In this case, the background picture is on the desktop, on a reall app the bg would be in the same folder as the program
 screen.bgpic(picname="C:\\Users\\Giabbi\\Desktop\\bg.gif") 
 
-commandQueue = Queue()
+commandQueue = Queue() # Queue to process turtle comands in the main tread since tkinter is not Thread safe
 
-writeEvents = Turtle()
+writeEvents = Turtle() # Create a pen to write events on the screen
 writeEvents.hideturtle()
 writeEvents.penup()
 writeEventsY = 200
@@ -42,29 +42,29 @@ def car1Handler(dimensions, luck, nitro, power):
                                                       str(dimensions) + " " + str(luck) + " " + str(nitro) + " " + str(power), 
                                                       get_pty = True) # Change directory and start the program in car 1, get_pty = True is to reduce lag
             jump = False
-
-        eventStopper = False
+            
         for line in iter(stdout.readline, ""): # Read bytes as they are sent from Car 1 
-            '''global shell1
-            if shell1 and "Car 2 got a power star! It has become invulnerable to crashes!" not in line and not eventStopper:
-                stdin1, stdout1, stderr2 = car1.exec_command("cd \"bcm2835-1.70/Motor_Driver_HAT_Code/Motor_Driver_HAT_Code/Raspberry Pi/python\"\n python3 test.py", get_pty = True)
-                eventStopper = True
-                jump = True
-                break'''
+            global shell1
+            if shell1 and "Car 2 got a power star! It has become invulnerable to crashes!" not in line: # Check if a shell has been thrown and if the current car has a power star
+                stdin1, stdout1, stderr2 = car1.exec_command("cd \"bcm2835-1.70/Motor_Driver_HAT_Code/Motor_Driver_HAT_Code/Raspberry Pi/python\"\n python3 test.py", get_pty = True) # Test.py is stop.py, executing it kills the car for a few seconds
+                jump = True # Restart SSH connection
+                car1.close()
+                break
         
             if "A shell was thrown by Car 1 to Car 2!" in line:
-                continue
-            commandQueue.put(lambda: writeEvents.write(line, align = "center", font = ("Britannic Bold", 10, "normal")))
+                shell2 = True # Throw the shell to Car 2
+                
+            commandQueue.put(lambda: writeEvents.write(line, align = "center", font = ("Britannic Bold", 10, "normal"))) # Write the events on the screen
             commandQueue.put(lambda: screen.update())
             if writeEventsY > -100:
                 writeEventsY -= 30
-                commandQueue.put(lambda: writeEvents.goto(-250, writeEventsY))
+                commandQueue.put(lambda: writeEvents.goto(-250, writeEventsY)) # Move the next event to prevent overlapping
             else:
-                commandQueue.put(lambda: writeEvents.clear())
+                commandQueue.put(lambda: writeEvents.clear()) # If events have gone too much down then clear the past events and bring the pen up
                 writeEventsY = 200
                 commandQueue.put(lambda: writeEvents.goto(-250, writeEventsY))
 
-            if "Car 1 crashed!!!" in line: # Check if the car crashed
+            if "Car 1 crashed!!!" in line: # Check if the car crashed, on a crash a box would appear and the user has to click it, afterwards paramiko would re-run the program on the rpi
                 print("Quick! Fix Car 1 before Car 2 wins, click the blue box to fix")
                 restart = Turtle("square")
                 commandQueue.put(lambda: restart.penup())
@@ -76,17 +76,17 @@ def car1Handler(dimensions, luck, nitro, power):
                 commandQueue.put(lambda: screen.update())
                 if writeEventsY > -100:
                     writeEventsY -= 30
-                    commandQueue.put(lambda: writeEvents.goto(-250, writeEventsY))
+                    commandQueue.put(lambda: writeEvents.goto(-250, writeEventsY)) # Move the next event to prevent overlappi
                 else:
                     commandQueue.put(lambda:  writeEvents.clear())
                     writeEventsY = 200
-                    commandQueue.put(lambda: writeEvents.goto(-250, writeEventsY))
+                    commandQueue.put(lambda: writeEvents.goto(-250, writeEventsY)) # If events have gone too much down then clear the past events and bring the pen up
                 jump = True # Jump at the beginning of the loop and restart the SSH connection
             elif "Car 1 finished the race!" in line:
                 global car1Finish
                 car1Finish = True
-                car1.close()
-                return
+                car1.close() # Close SSH connection
+                return # Return Thread when the car finishes the race
 
 
 def car2Handler(dimensions, luck, nitro, power):
@@ -99,20 +99,21 @@ def car2Handler(dimensions, luck, nitro, power):
             car2.connect(username = "giabbi", hostname = "ambropi.local", password = "Limatola1!") # SSH into Car 2's pi
             stdin, stdout, stderr = car2.exec_command("cd \"bcm2835-1.70/Motor_Driver_HAT_Code/Motor_Driver_HAT_Code/Raspberry Pi/python\"\npython3 main.py " + 
                                                       str(dimensions) + " " + str(luck) + " " + str(nitro) + " " + str(power), 
-                                                      get_pty = True) # Change directory and start the program in car 1, get_pty = True is to reduce lag
+                                                      get_pty = True) # Change directory and start the program in car 2, get_pty = True is to reduce lag
             jump = False
 
-        eventStopper = False
         for line in iter(stdout.readline, ""): # Read bytes as they are sent from Car 2 
-            ''' global shell2
-            if shell2 and "Car 2 got a power star! It has become invulnerable to crashes!" not in line and not eventStopper:
-                stdin1, stdout1, stderr2 = car2.exec_command("cd \"bcm2835-1.70/Motor_Driver_HAT_Code/Motor_Driver_HAT_Code/Raspberry Pi/python\"\n python3 test.py", get_pty = True)
-                eventStopper = True
-                jump = True
-                break '''
+             global shell2
+            if shell2 and "Car 2 got a power star! It has become invulnerable to crashes!" not in line: # Check if a shell has been thrown and if the current car has a power star
+                stdin1, stdout1, stderr2 = car2.exec_command("cd \"bcm2835-1.70/Motor_Driver_HAT_Code/Motor_Driver_HAT_Code/Raspberry Pi/python\"\n python3 test.py", get_pty = True) # Test.py is stop.py here on github, executing it kills the car for a few seconds
+                jump = True # Restart SSH connection
+                car2.close()
+                break 
 
             if "A shell was thrown by Car 2 to Car 1!" in line:
-                continue
+                shell1 = True # Throw the shell to Car 1
+
+            # The following part is identical to car 1, for comments check car1Handler()
             commandQueue.put(lambda: writeEvents.write(line, align = "center", font = ("Britannic Bold", 10, "normal")))    
             commandQueue.put(lambda: screen.update())
             if writeEventsY > -100:
@@ -123,7 +124,7 @@ def car2Handler(dimensions, luck, nitro, power):
                 writeEventsY = 200
                 commandQueue.put(lambda: writeEvents.goto(-250, writeEventsY))
 
-            if "Car 2 crashed!!!" in line: # Check if the car crashed
+            if "Car 2 crashed!!!" in line: 
                 print("Quick! Fix Car 2 before Car 1 wins, click the green box to fix")
                 restart = Turtle("square")
                 commandQueue.put(lambda: restart.penup())
@@ -140,7 +141,7 @@ def car2Handler(dimensions, luck, nitro, power):
                     commandQueue.put(lambda: writeEvents.clear())
                     writeEventsY = 200
                     commandQueue.put(lambda: writeEvents.goto(-250, writeEventsY))
-                jump = True # Jump at the beginning of the loop and restart the SSH connection
+                jump = True 
             elif "A shell was thrown by Car 2 to Car 1!" in line:
                 global shell1
                 shell1 = True
@@ -157,7 +158,7 @@ def process_queue():
 
 
 def main():
-    # Car 1 attributes
+    # Car 1 attributes prompts
     
     dimensions1 = int(screen.numinput("Select attributes for Car 1", "Choose the dimension for the car, 1 = very small 10 = the biggest",
                                        minval = 1, maxval = 10))
@@ -179,7 +180,7 @@ def main():
     if power1 == None:
         screen.bye() 
 
-    # Car 2 attribuites
+    # Car 2 attribuites prompts
     
     dimensions2 = int(screen.numinput("Select attributes for Car 2", "Choose the dimension for the car, 1 = very small 10 = the biggest",
                                        minval = 1, maxval = 10))
@@ -209,6 +210,7 @@ def main():
     car1Thread.start()
     car2Thread.start()
 
+    # Create a ready, set, go traffic light; the timing of sleep() was calculated according to my and the school's internet speed
     ready = Turtle()
     ready.penup()
     ready.goto(-200, 60)
@@ -255,6 +257,7 @@ def main():
 
     sleep(1)
 
+    # Clear traffic light for writeEvents Turtle
     ready.clear()
     ready.hideturtle()
     set.clear()
@@ -265,14 +268,14 @@ def main():
     screen.update()
 
     global car1Finish, car2Finish
-    while not car1Finish and not car2Finish:
+    while not car1Finish and not car2Finish: # Process the Queue in the main Threads with Turtle commands from both Car threads until both car finish the race
         process_queue()
 
     # Wait until both Threads return
     car1Thread.join()
     car2Thread.join()
     done()
-    screen.bye()
+    screen.bye() # When the race is over allow the user to close the screen
 
 if __name__ == "__main__":
     main()
